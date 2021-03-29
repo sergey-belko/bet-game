ec0d5c52f8f993aef0d35a91b0f83b3c360d64d4
Siarhei Belko <siarhei.belko@crunch.uk>
2021-03-29 11:56:52 +0300

    fix problem with rerendering/navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,29 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
 import logo from './logo.svg';
-import { connect, sendMessage } from './websocket';
+import { connect, sendMessage, disconnect } from './websocket';
 
 
 function App() {
   const [greeting, setGreeting] = useState();
 
+  const [bet, setBet] = useState(0);
+  const [guess, setGuess] = useState(0);
+
   useEffect(() => {
     connect(onMessageReceived)
+    fetch("http://localhost:8080/score/list", {
+      method: 'GET',
+
+    })
+      .then(res => res.json())
+      .then(setGreeting)
+      .catch(console.error)
+
+    return disconnect
   }, []);
 
-  const onMessageReceived = (msg) => {
-    const notification = JSON.parse(msg.body);
-
+  const onMessageReceived = (notification) => {
     console.log("Received a new message: ", { notification });
     setGreeting(notification)
   };
@@ -22,23 +32,42 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {greeting ? (
-          <p>Hello from {greeting}</p>
-        ) : (
-            <p>Loading...</p>
-          )}
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p> Let's play, Neo. Try to guess number </p>
+        <p>Place your bet and guess the number between 1 and 10</p>
+        <form>
+          <input
+            placeholder='Your bet here'
+            value={bet}
+            onChange={e => {
+              setBet(e.target.value)
+              e.preventDefault()
+            }}
+          />
+          <input
+            placeholder='Your guess here'
+            onChange={e => {
+              setGuess(e.target.value)
+              e.preventDefault()
+            }}
+          />
 
-        <button
-          onClick={() => {
-            sendMessage("AAAAAAAAAAAAAAAAA")
-            setGreeting(false)
-          }}
-        >
-        Send message
+          {greeting ? (
+            <p>Hello from {greeting}</p>
+          ) : (
+              <p>Loading...</p>
+            )}
+
+          <button
+            onClick={(e) => {
+              console.log({ bet, guess })
+              sendMessage({ bet, guess })
+              setGreeting(false)
+              e.preventDefault()
+            }}
+          >
+            SUBMIT
         </button>
+        </form>
       </header>
     </div>
   );
--- frontend/src/websocket.js
+++ frontend/src/websocket.js
@@ -2,17 +2,19 @@ import Stomp from "stompjs"
 import SockJS from "sockjs-client"
 
 const HOST = "http://localhost:8080"
-const CONNECT_API = HOST + '/subscribe'
-const NOTIFY_API = '/topic/greetings'
-const COMMAND_API = "/app/hello"
+const API_URL = HOST + '/subscribe'
+const NOTIFY_API_CHANNEL = '/notify/vote'
+const SEND_API_CHANNEL = "/casino/vote/submit"
 
 let stompClient = null;
 
 export const connect = (onMessageReceived) => {
-    stompClient = Stomp.over(new SockJS(CONNECT_API));
+    stompClient = Stomp.over(new SockJS(API_URL));
     stompClient.connect({}, (response) => {
         console.log('Connected: ' + response)
-        stompClient.subscribe(NOTIFY_API, onMessageReceived)
+        stompClient.subscribe(NOTIFY_API_CHANNEL, (event) => {
+            onMessageReceived(event.body)
+        })
     }, onError)
 }
 
@@ -25,7 +27,7 @@ export const disconnect = () => {
 
 export const sendMessage = (name) => {
     if (stompClient !== null) {
-        stompClient.send(COMMAND_API, JSON.stringify(name))
+        stompClient.send(SEND_API_CHANNEL, {}, JSON.stringify(name))
     }
 }
 
